a191c338143571cc5a75a188580958cd1e48ef71
Kevin Do <v946u526@wichita.edu>
2022-09-24 18:50:23 -0500

    Added our names/project name at top and Options

--- Hotel_Reservation/Hotel Reservation Solution/Hotel Reservation Project/Main Form.vb
+++ Hotel_Reservation/Hotel Reservation Solution/Hotel Reservation Project/Main Form.vb
@@ -1,4 +1,12 @@
-﻿Public Class frmMain
+﻿'Hotel Reservation Project
+'Kevin Do and Tyler Wheeler
+'September 25, 2022
+
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Public Class frmMain
     Private Sub Label19_Click(sender As Object, e As EventArgs) Handles Label19.Click
 
     End Sub
