aedcfd994bf66690d4f1a2921e1cc189f7c9759e
Kevin Do <v946u526@wichita.edu>
2022-09-24 18:58:54 -0500

    Added all the Constants in with numbers

--- Hotel_Reservation/Hotel Reservation Solution/Hotel Reservation Project/Main Form.vb
+++ Hotel_Reservation/Hotel Reservation Solution/Hotel Reservation Project/Main Form.vb
@@ -8,6 +8,18 @@ Option Strict On
 
 Public Class frmMain
     Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
-
+        Const dblDBLMember As Double = 140
+        Const dblDBLRegular As Double = 145
+        Const dblKingMember As Double = 150
+        Const dblKingRegular As Double = 160
+        Const dblQueenMember As Double = 170
+        Const dblQueenRegular As Double = 180
+        Const intMaxOccupancy As Integer = 5
+        Const dblFee As Double = 12.5
+        Const dblPromo As Double = 0.15
+        Const dblTaxRate As Double = 0.1425
+        Const dblAAA As Double = 0.06
+        Const dblCorporate As Double = 0.08
+        Const dblGov As Double = 0.1
     End Sub
 End Class
